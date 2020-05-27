735d6327f19e79f3cf94b95b68b59637c75cf0e3
fcombes <frederic.combes@i-run.fr>
2020-05-27 18:44:24 +0200

    test(openapi): Add integration tests before refactor
    [skip ci]

---
+++ src/test/java/fr/irun/openapi/swagger/readers/OpenAPIReaderFullTest.java
@@ -0,0 +1,52 @@
+package fr.irun.openapi.swagger.readers;
+
+import com.google.common.io.CharStreams;
+import fr.irun.openapi.swagger.samples.RestWithBodyController;
+import fr.irun.openapi.swagger.samples.RestWithConsumesController;
+import fr.irun.openapi.swagger.samples.SimpleRestController;
+import fr.irun.openapi.swagger.samples.SimpleRestWithParameters;
+import io.swagger.v3.core.util.Json;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import net.javacrumbs.jsonunit.assertj.JsonAssertions;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class OpenAPIReaderFullTest {
+    private SpringOpenApiReader tested;
+
+    @BeforeEach
+    void setUp() {
+        tested = new SpringOpenApiReader(new OpenAPI()
+                .info(new Info()
+                        .title("test")
+                        .version("1.0.0")));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {
+            RestWithBodyController.class,
+            RestWithConsumesController.class,
+            SimpleRestController.class,
+            SimpleRestWithParameters.class
+    })
+    void should_generate_yaml_for_class(Class<?> clazz) throws IOException {
+        OpenAPI openAPI = tested.read(clazz);
+
+        String actual = Json.pretty(openAPI);
+
+        Assertions.assertThat(actual).isNotEmpty();
+
+        InputStream resourceAsStream = OpenAPIReaderFullTest.class.getClassLoader().getResourceAsStream("openapi-yamls/" + clazz.getSimpleName() + ".json");
+        Assertions.assertThat(resourceAsStream).isNotNull();
+        String expectedJson = CharStreams.toString(new InputStreamReader(resourceAsStream, StandardCharsets.UTF_8));
+        JsonAssertions.assertThatJson(expectedJson).isEqualTo(expectedJson);
+    }
+}
